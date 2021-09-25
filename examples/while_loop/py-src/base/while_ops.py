dbf9e2af5e27b3edf81940ca85a2f74dedea08fe
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2021-09-25 18:50:33 +0200

    Manually wrote the while loop example in Python.

---
+++ examples/while_loop/py-src/base/while_ops.py
@@ -0,0 +1,16 @@
+from collections import namedtuple
+
+def while_ops(_State, _Context, _cond, _body):
+    def body(state, context):
+        return _body(state, context)
+    
+    def cond(state, context):
+        return _cond(state, context)
+
+    def repeat(state, context):
+        while cond(state, context):
+            body(state, context)
+
+    while_ops_tuple = namedtuple('while_ops',
+                                 ['State', 'Context', 'body', 'cond', 'repeat'])
+    return while_ops_tuple(_State, _Context, body, cond, repeat)
