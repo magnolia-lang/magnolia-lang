44a9bb5488127e9dcd4520ea3935473ec02e661c
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2021-11-18 10:00:17 +0100

    Small change to testcase

--- examples/bgl_v2/cpp-src/testcase.hpp
+++ examples/bgl_v2/cpp-src/testcase.hpp
@@ -5,6 +5,6 @@
 #include <utility>
 
 #define NB_TEST_VERTICES 1000000
-#define NB_TEST_EDGES 10000000
+#define NB_TEST_EDGES 30000000
 
 std::pair<int, std::list<std::pair<int, int>>> gen_test_case();
