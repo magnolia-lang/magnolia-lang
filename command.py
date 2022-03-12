47e8fcd43b024d804203a60cb9f48c7503e24b90
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2022-03-12 21:21:17 +0100

    Convenient command file to run experiments

---
+++ command.py
@@ -0,0 +1,33 @@
+import subprocess
+
+magnolia = 'cabal exec -v0 magnolia --'
+
+def run_compiler(rewrite_confs):
+    package_name = "examples.pde.mg-src.pde-cpp"
+    command_line = [magnolia, "build"]
+    command_line += ["-o", "examples/pde/cpp-src/gen"]
+    command_line += ["--backend", "cpp"]
+    command_line += ["--base-import-directory", "gen"]
+    command_line += ["--allow-overwrite"]
+    command_line.append("examples/pde/mg-src/pde-cpp.mg")
+    command_line += ["--programs-to-rewrite",
+                     f"{package_name}.PDEProgram"]
+    command_line += ["--rewriting-system-configs",
+                     "'" + ','.join(f"{package_name}.{name}|{steps}" for name,steps in rewrite_confs) + "'"]
+
+    command_line = [' '.join(command_line)]
+    process = subprocess.run(command_line, capture_output=True, shell=True)
+    print(process.returncode)
+    print(process.stderr.decode('utf-8'))
+    print(process.stdout.decode('utf-8'))
+
+if __name__ == '__main__':
+    rew_configs = [ ("DNFRules", 100)
+                  , ("OFIntroducePaddingInArguments", 0)
+                  #, ("OFIntroducePaddingRule", 1)
+                  #, ("OFAddLeftPadding0Axis", 1)
+                  #, ("OFAddRightPadding0Axis", 1)
+                  #, ("OFExtractInnerRule", 1)
+                  #, ("OFRemoveLeftoverPadding", 100)
+                  ]
+    run_compiler(rew_configs)
