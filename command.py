da036c7dc2fa3a354510dc2ab644ffe05922e7ac
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2022-03-19 14:04:31 +0100

    New "optims"

--- command.py
+++ command.py
@@ -22,13 +22,16 @@ def run_compiler(rewrite_confs):
     print(process.stdout.decode('utf-8'))
 
 if __name__ == '__main__':
-    rew_configs = [ ("DNFRules", 100)
-                  , ("OFRavel", 1)
-                  , ("ShapeIsUnique", 100)
-                  , ("ONFToArrayRules", 100)
-                  , ("PsiCorrespondenceTheorem", 1)
-                  , ("ShapeIsUnique", 100) # Could rewrite next rewrite rules
-                  #, ("OFTile", 1)
+    rew_configs = [ ##("DNFIntroducePsi", 100)
+                  ##, ("DNFCleanupPsiIntroduction", 100)
+                    ("DNFRules", 100)
+                  ##, ("OFRavel", 1)
+                  ##, ("ShapeIsUnique", 100) # WRONG SHAPE IS NOT UNIQUE
+                  ##, ("ONFToArrayRules", 100)
+                  ##, ("PsiCorrespondenceTheorem", 1)
+                  ##, ("ShapeIsUnique", 100) # Could rewrite next rewrite rules
+                  ##, ("OFCleanupIdentity", 100)
+                  , ("OFTile", 1)
                   #, ("OFLiftCores", 1)
                   ##, ("OFIntroducePaddingInArguments", 1)
                   #, ("OFIntroducePaddingRule", 1)
