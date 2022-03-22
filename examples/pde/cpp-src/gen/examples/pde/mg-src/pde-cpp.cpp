17be700c9463a53a1131a15b22144531c66a9acb
Benjamin Chetioui <chetioui.benjamin@gmail.com>
2022-03-22 16:02:20 +0100

    Gotta go fast!!

--- examples/pde/cpp-src/gen/examples/pde/mg-src/pde-cpp.cpp
+++ examples/pde/cpp-src/gen/examples/pde/mg-src/pde-cpp.cpp
@@ -13,41 +13,7 @@ namespace pde_cpp {
 
     PDEProgram::_one PDEProgram::one;
 
-    hardware_info PDEProgram::__hardware_info;
-
     array_ops PDEProgram::__array_ops;
-
-
-
-    PDEProgram::_uniqueShape PDEProgram::uniqueShape;
-
-
-
-    PDEProgram::_iota PDEProgram::iota;
-
-
-
-    PDEProgram::_nbCores PDEProgram::nbCores;
-
-
-
-    PDEProgram::_elementsAt PDEProgram::elementsAt;
-
-
-
-    PDEProgram::_emptyIndex PDEProgram::emptyIndex;
-
-
-
-    PDEProgram::_start PDEProgram::start;
-
-
-
-    PDEProgram::_stride PDEProgram::stride;
-
-
-
-    PDEProgram::_subshape PDEProgram::subshape;
 
 
 
@@ -59,23 +25,11 @@ namespace pde_cpp {
 
 
 
-    PDEProgram::_cpadl PDEProgram::cpadl;
-
-
-
-    PDEProgram::_cpadr PDEProgram::cpadr;
-
-
-
     PDEProgram::_rotate_ix PDEProgram::rotate_ix;
 
 
 
     PDEProgram::_zero PDEProgram::zero;
-
-
-
-    PDEProgram::_asPadded PDEProgram::asPadded;
 
 
 
@@ -95,23 +49,7 @@ namespace pde_cpp {
 
 
 
-    PDEProgram::_forall_ix_snippet_padded PDEProgram::forall_ix_snippet_padded;
-
-
-
-    PDEProgram::_forall_ix_snippet_threaded PDEProgram::forall_ix_snippet_threaded;
-
-
-
-    PDEProgram::_forall_ix_snippet_tiled PDEProgram::forall_ix_snippet_tiled;
-
-
-
     PDEProgram::_mul PDEProgram::mul;
-
-
-
-    PDEProgram::_padAndLift PDEProgram::padAndLift;
 
 
 
@@ -119,15 +57,7 @@ namespace pde_cpp {
 
 
 
-    PDEProgram::_rav PDEProgram::rav;
-
-
-
     PDEProgram::_rotate PDEProgram::rotate;
-
-
-
-    PDEProgram::_shape PDEProgram::shape;
 
 
 
@@ -135,21 +65,13 @@ namespace pde_cpp {
 
 
 
-    forall_ops<PDEProgram::Array, PDEProgram::Axis, PDEProgram::Float, PDEProgram::Index, PDEProgram::Nat, PDEProgram::Offset, PDEProgram::PaddedArray, PDEProgram::_snippet_ix> PDEProgram::__forall_ops;
+    forall_ops<PDEProgram::Array, PDEProgram::Axis, PDEProgram::Float, PDEProgram::Index, PDEProgram::Nat, PDEProgram::Offset, PDEProgram::_snippet_ix> PDEProgram::__forall_ops;
 
     PDEProgram::_snippet_ix PDEProgram::snippet_ix;
 
 
 
     PDEProgram::_step PDEProgram::step;
-
-
-
-    PDEProgram::_toArray PDEProgram::toArray;
-
-
-
-    PDEProgram::_unliftAndUnpad PDEProgram::unliftAndUnpad;
 
 } // examples
 } // pde
--- examples/pde/cpp-src/main.cpp
+++ examples/pde/cpp-src/main.cpp
@@ -3,7 +3,7 @@
 #include "gen/examples/pde/mg-src/pde-cpp.hpp"
 #include "base.hpp"
 
-typedef array_ops::Shape Shape;
+//typedef array_ops::Shape Shape;
 typedef array_ops::Array Array;
 typedef array_ops::Index Index;
 typedef examples::pde::mg_src::pde_cpp::PDEProgram PDEProgram;
@@ -13,11 +13,17 @@ static const double s_nu = 1.0;
 static const double s_dx = 1.0;
 
 int main() {
-    size_t side = SIDE; //256; 
-    size_t steps = 1;
-    Shape shape = Shape(std::vector<size_t>({ side, side, side }));
-    Array u0 = dumpsine(shape), u1 = dumpsine(shape), u2 = dumpsine(shape);
-    //for (size_t i = 0; i < steps; ++i) {
+    size_t side = SIDE; //256;
+    size_t steps = 50;
+    //Shape shape = Shape(std::vector<size_t>({ side, side, side }));
+    Array u0, u1, u2;
+    dumpsine(u0);
+    dumpsine(u1);
+    dumpsine(u2);
+    for (size_t i = 0; i < steps; ++i) {
         PDEProgram::step(u0, u1, u2, s_nu, s_dx, s_dt);
-    //}
+        std::cout << u0[0] << " "
+                  << u1[0] << " "
+                  << u2[0] << std::endl;
+    }
 }
